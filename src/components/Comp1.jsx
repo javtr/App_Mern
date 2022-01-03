c0db0085deff88373599f9bb713238601b5eda68
javier triana <javtriana.z@gmail.com>
2022-01-03 18:00:00 -0500

    start

---
+++ src/components/Comp1.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Salida() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h2>Este es un subtitulo</h2>
+    </div>
+  );
+}
+
+export function OtraSalida() {
+  return (
+    <div>
+      <h1>Otra salida </h1>
+    </div>
+  );
+}
