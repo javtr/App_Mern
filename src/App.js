f0352c6d8943942d525f99cf2b8a998ac19aa065
javier triana <javtriana.z@gmail.com>
2022-01-06 18:07:50 -0500

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/Saludar'
-import Saludar from './components/Saludar';
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/Saludar";
+import Saludar from "./components/Saludar";
 
 function App() {
   let userInfo = {
-    // name: "Javier Triana",
-    age: "32"
-  }
+    name: "Javier Triana",
+    age: "32",
+  };
 
-  const saludarFn = name => {
-   alert(name);
-  }
+  const saludarFn = (nameFn, ageFn) => {
+    alert(`Hola ${nameFn}, tienes ${ageFn} años`);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
-        <Saludar userInfo={userInfo}  saludarFn={saludarFn}  />
-        
+        <Saludar userInfo={userInfo} saludarFn={saludarFn} />
       </header>
     </div>
   );
--- src/components/Saludar.jsx
+++ src/components/Saludar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function Saludar(props) {
 
 let {userInfo, saludarFn} = props;
-let {name = "valor default", age} = userInfo;
+let {name = "valor default", age = "00"} = userInfo;
 
   return (
     <div>
       <h2>Hola {name}</h2>
-      <button onClick={() => saludarFn(name)}>Saludar</button>
+      <button onClick={() => saludarFn(name,age)}>Saludar</button>
     </div>
   );
 }
